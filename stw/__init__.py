35faaa9570dca3bb940e8891cc2529ce22b7b924
JJOP01 <jjo.pumipi@gmail.com>
2026-06-20 01:39:01 +1200

    added CLI configed through pyproject.toml, configed .venv, added requirements

---
+++ stw/__init__.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    print("Hello, this argparse works!")    
+
+if __name__ == "__main__":
+    main()
---
+++ stw/site-configs.py
@@ -0,0 +1,12 @@
+DEFAULT_SITE = {
+    
+}
+
+SITE_CONFIG = {
+    "website1.com": {
+        
+    },
+    "website2.com": {
+        
+    },
+}
