d3453a217f45a04667e74c786de4e5d130b185a8
JJOP01 <jjo.pumipi@gmail.com>
2026-07-05 23:59:37 +1200

    changed downloader style

--- stw/downloader/__init__.py
+++ stw/downloader/__init__.py
@@ -1,7 +1,18 @@
 from .common import FileDownloader
 from .http import HttpFD
 
+PROTOCOL_MAP = {
+    'http': HttpFD
+}
+
+def get_suitable_downloader(info):
+    protocol = info["protocol"]
+    try:
+        return PROTOCOL_MAP[protocol]
+    except KeyError:
+        raise ValueError(f"Unknown protocol: {protocol}")
 
 __all__ = [
     "FileDownloader",
+    "get_suitable_downloader"
 ]
